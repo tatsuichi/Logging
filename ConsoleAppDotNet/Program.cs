feef26e605f181f2fe54bf60d69c12ef4fa9df75
tatsuichi <tatsuri1984@gmail.com>
2023-03-03 11:41:26 +0900

    VisualStudioの「コンソールアプリケーション」の初期コード

---
+++ ConsoleAppDotNet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
