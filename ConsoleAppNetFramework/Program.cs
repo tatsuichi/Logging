1a3471259a2d4eb8053d31572da144e359b42451
tatsuichi <tatsuri1984@gmail.com>
2023-03-03 17:11:26 +0900

    NLogの対応

--- ConsoleAppNetFramework/Program.cs
+++ ConsoleAppNetFramework/Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleAppNetFramework
 {
     internal class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            try
+            {
+                Logger.Info("Hello world");
+                System.Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Goodbye cruel world");
+            }
         }
     }
 }
