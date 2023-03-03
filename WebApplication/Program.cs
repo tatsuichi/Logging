2cc64272e58d4f24a74f5493147357ac8587b26f
tatsuichi <tatsuri1984@gmail.com>
2023-03-03 17:22:46 +0900

    NLogの対応

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,25 +1,48 @@
+using NLog;
+using NLog.Web;
+
 namespace WebApplication
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Debug("init main");
+            try
+            {
+                var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+                // Add services to the container.
 
-            builder.Services.AddControllers();
+                builder.Services.AddControllers();
 
-            var app = builder.Build();
+                // NLog: Setup NLog for Dependency injection
+                builder.Logging.ClearProviders();
+                builder.Host.UseNLog();
 
-            // Configure the HTTP request pipeline.
+                var app = builder.Build();
 
-            app.UseAuthorization();
+                // Configure the HTTP request pipeline.
+
+                app.UseAuthorization();
 
 
-            app.MapControllers();
+                app.MapControllers();
 
-            app.Run();
+                app.Run();
+            }
+            catch (Exception exception)
+            {
+                // NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
     }
 }
