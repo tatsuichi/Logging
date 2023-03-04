ee658a3d8c17f26aef0e72b1b65b6fd199ae9faa
tatsuichi <tatsuri1984@gmail.com>
2023-03-04 16:07:29 +0900

    NLogの対応

--- ConsoleAppDotNet/Program.cs
+++ ConsoleAppDotNet/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace ConsoleAppDotNet
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace ConsoleAppDotNet
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                using var servicesProvider = new ServiceCollection()
+                    .AddTransient<Runner>() // Runner is the custom class
+                    .AddLogging(loggingBuilder =>
+                    {
+                        // configure Logging with NLog
+                        loggingBuilder.ClearProviders();
+                        loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                        loggingBuilder.AddNLog();
+                    }).BuildServiceProvider();
+
+                var runner = servicesProvider.GetRequiredService<Runner>();
+                runner.DoAction("Action1");
+
+                Console.WriteLine("Press ANY key to exit");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                // NLog: catch any exception and log it.
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
         }
     }
 }
