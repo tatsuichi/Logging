6cfece3bcd1f43dbbac5f254e3be92684c9c9791
tatsuichi <tatsuri1984@gmail.com>
2023-03-04 16:12:35 +0900

    NLogの対応

--- ConsoleAppDotNet/Program.cs
+++ ConsoleAppDotNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Extensions.Logging;
@@ -12,6 +13,11 @@ namespace ConsoleAppDotNet
             var logger = LogManager.GetCurrentClassLogger();
             try
             {
+                var config = new ConfigurationBuilder()
+                   .SetBasePath(System.IO.Directory.GetCurrentDirectory()) //From NuGet Package Microsoft.Extensions.Configuration.Json
+                   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                   .Build();
+
                 using var servicesProvider = new ServiceCollection()
                     .AddTransient<Runner>() // Runner is the custom class
                     .AddLogging(loggingBuilder =>
@@ -19,7 +25,7 @@ namespace ConsoleAppDotNet
                         // configure Logging with NLog
                         loggingBuilder.ClearProviders();
                         loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                        loggingBuilder.AddNLog();
+                        loggingBuilder.AddNLog(config);
                     }).BuildServiceProvider();
 
                 var runner = servicesProvider.GetRequiredService<Runner>();
